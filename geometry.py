3b1fcab715b823306560f83769e652ff185180ed
raj89er@gmail.com <raj89er@gmail.com>
2024-03-13 14:52:05 -0400

    first commit

---
+++ geometry.py
@@ -0,0 +1,9 @@
+import math
+
+# Caclc area of circle
+def circle_area(radius):
+    return math.pi * radius ** 2
+
+# Calc hypotenuse from 2 sides
+def hyp(a, b):
+    return math.sqrt(a ** 2 + b ** 2)
